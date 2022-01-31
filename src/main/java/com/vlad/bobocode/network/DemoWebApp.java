fa63f5b462332d78d1afb599f309e46c089e68d8
Vladyslav Paun <paunvlad122@gmail.com>
2022-01-31 11:52:08 +0100

    [BOBOCODE] socket + multithreading

---
+++ src/main/java/com/vlad/bobocode/network/DemoWebApp.java
@@ -0,0 +1,22 @@
+package com.vlad.bobocode.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+
+public class DemoWebApp {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(8899)) {
+            while (true) {
+                try (var cleint = serverSocket.accept()) {
+                    var reader = new BufferedReader(new InputStreamReader(cleint.getInputStream()));
+                    var line = reader.readLine();
+                    System.out.println(cleint.getInetAddress().getHostAddress() + " - " + line);
+                }catch (Exception e) {
+                    e.printStackTrace();
+            }
+        }
+        }
+    }
+}
